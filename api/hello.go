6b1770eeeb3d2e65ce77f00e774a3ee0530635a1
Sergey_N <sergej2novikov@gmail.com>
2023-07-21 16:41:10 +0300

    12

--- api/hello.go
+++ api/hello.go
@@ -11,7 +11,5 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	  if err != nil {
 			 fmt.Printf("server: could not read request body: %s\n", err)
 	  }
-	fmt.Fprintf(w, `<!DOCTYPE html><html lang="ru"><head><meta charset="UTF-8"></head><body>`)
-	fmt.Fprintf(w,string(reqBody))
-	fmt.Fprintf(w, ` </body></html>')
+	fmt.Fprintf(w,"%s",string(reqBody))
 }
